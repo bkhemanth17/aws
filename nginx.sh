f5cf68988527c88561bcc59e4bec6e76bd401b8e
bkhemanth17 <146440776+bkhemanth17@users.noreply.github.com>
2024-03-16 14:48:37 +0530

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -1,4 +1,15 @@
+#install nginx server in the server after vm creation
 #!/bin/bash
 yum update -y
 amazon-linux-extras install nginx1.12 -y
 service nginx start
+/usr/share/nginx/html/index.html   #path to access the web server html file
+
+
+#installing a web server(nginx server) in linux machine at vm creation level.
+#!/bin/bash
+yum update -y
+amazon-linux-extras install nginx1.12
+service nginx start
+echo '<h1 WEB SERVER-1</h1>' >>
+/usr/share/nginx/html/index.html
